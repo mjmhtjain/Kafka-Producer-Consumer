4d27f99b53b478608f1699e4fbc6f72be212ea16
mohitjain@optum.com <mohitjain@optum.com>
2021-01-03 22:32:57 +0530

    Producers

---
+++ src/main/java/com/kafka/tutorial1/ProducerDemoKeys.java
@@ -0,0 +1,55 @@
+package com.kafka.tutorial1;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoKeys {
+    static String BOOTSTRAP_SERVERS = "127.0.0.1:9092";
+
+    static Logger logger = LoggerFactory.getLogger(ProducerDemoKeys.class);
+
+    public static void main(String[] args) {
+        //create producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //create producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        for (int i = 0; i < 10; i++) {
+            String key = "id_" + i;
+            String msg = "java-message-" + i;
+
+            //send kafka
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<>("first_topic", key, msg);
+            producer.send(producerRecord, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata metadata, Exception exception) {
+                    if (exception == null) {
+                        logger.info("Logger metadata" +
+                                        "Topic: {}" +
+                                        "Partition: {}" +
+                                        "Offset: {}" +
+                                        "Timestamp: {}",
+                                metadata.topic(),
+                                metadata.partition(),
+                                metadata.offset(),
+                                metadata.timestamp());
+                    } else {
+                        logger.error("this gave an exception: " + exception);
+                    }
+                }
+            });
+        }
+
+
+        producer.flush();
+        producer.close();
+    }
+}
